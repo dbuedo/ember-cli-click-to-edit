960d1e27dd95589b4f67118420d97c657a164d4b
David Buedo <davidbuedo@gmail.com>
2015-10-25 20:42:05 +0100

    removed console.log lines

--- addon/components/click-to-edit.js
+++ addon/components/click-to-edit.js
@@ -8,17 +8,14 @@ export default Ember.Component.extend({
   isEditing: false,
   
   click: function() {
-    console.log('click! on click-to-edit');
     this.set('isEditing', true);
     Ember.run.scheduleOnce('afterRender', this, 'setFocus');
   },
   setFocus: function() {
-	console.log('setting focus');
-    this.$('textarea').focus();
+	this.$('textarea').focus();
   },
   actions: {
     blur: function() {
-      console.log("focus  out! on click-to-edit");
       this.set('isEditing', false);
     }
   }
