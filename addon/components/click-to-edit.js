6c9b33909c5cd12d7a3d93bc62d18713a180fab8
David Buedo <davidbuedo@gmail.com>
2015-10-25 17:07:07 +0100

    first version of the click-to-edit textarea component

---
+++ addon/components/click-to-edit.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+import layout from '../templates/components/click-to-edit';
+
+export default Ember.Component.extend({
+  layout: layout,
+  classNameBindings: ['isEditing'],
+  isEditing: false,
+  
+  click: function() {
+    console.log('click! on click-to-edit');
+    this.set('isEditing', true);
+    Ember.run.scheduleOnce('afterRender', this, 'setFocus');
+  },
+  setFocus: function() {
+	console.log('setting focus');
+    this.$('textarea').focus();
+  },
+  actions: {
+    blur: function() {
+      console.log("focus  out! on click-to-edit");
+      this.set('isEditing', false);
+    }
+  }
+});
---
+++ app/components/click-to-edit.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-click-to-edit/components/click-to-edit';
