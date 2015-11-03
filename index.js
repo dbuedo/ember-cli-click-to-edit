9d2ca0c49179be0bbf021a32b396d3484d339eed
David Buedo <davidbuedo@gmail.com>
2015-11-04 00:22:09 +0100

    added blueprint to install autosize dependency automatically

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports = {
   name: 'ember-cli-click-to-edit',
 
   included: function(app) {
+  	this._super.included(app);
+
     app.import('vendor/click-to-edit.css');
-    app.import('bower_components/autosize/dist/autosize.min.js');
+    app.import(app.bowerDirectory + '/autosize/dist/autosize.min.js');
   }
 };
